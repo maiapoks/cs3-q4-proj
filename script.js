d847d3ba3087eaca0a32b221f91cfbd7ade01dfa
djxiao-888 <jherusspereira6@gmail.com>
2023-05-26 22:25:54 +0800

    may inedit ako

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Event listener for the search form submission
-document.getElementById('searchForm').addEventListener('submit', function(event) {
+document.getElementById('searchForm').addEventListener('submit', function (event) {
     event.preventDefault();
     var destination = document.getElementById('destination').value;
     var checkIn = document.getElementById('checkIn').value;
@@ -9,70 +9,70 @@ document.getElementById('searchForm').addEventListener('submit', function(event)
 
 // Function to perform hotel search
 function performHotelSearch(destination, checkIn, checkOut) {
-  var database = [
-    {
-        name: 'Boracay Beach Resort',
-        location: 'Boracay',
-        price: 'p1336/night',
-        rating: 4.2,
-        description: 'A beautiful beachfront resort located in the heart of Boracay.',
-        image: 'https://example.com/boracay-beach-resort.jpg',
-        url: 'https://example.com/boracay-beach-resort'
-    },
-    {
-        name: 'Nigi Nigi Too',
-        location: 'Boracay',
-        price: 'p3051/night',
-        rating: 4.2,
-        description: 'A cozy beachfront hotel offering comfortable accommodations and stunning views.',
-        image: 'https://example.com/nigi-nigi-too.jpg',
-        url: 'https://example.com/nigi-nigi-too'
-    },
-    {
-        name: 'Alta Vista de Boracay',
-        location: 'Boracay',
-        price: 'p2407/night',
-        rating: 4.0,
-        description: 'Experience luxury and relaxation at this hilltop resort in Boracay.',
-        image: 'https://example.com/alta-vista-de-boracay.jpg',
-        url: 'https://example.com/alta-vista-de-boracay'
-    },
-    {
-        name: 'Chill-Out Hostel Boracay',
-        location: 'Boracay',
-        price: 'p1323/night',
-        rating: 4.2,
-        description: 'A budget-friendly hostel located just a few steps from the beach.',
-        image: 'https://example.com/chill-out-hostel.jpg',
-        url: 'https://example.com/chill-out-hostel'
-    },
-    {
-        name: 'El Nido Cliffside Cottages',
-        location: 'El Nido',
-        price: 'p897/night',
-        rating: 4.0,
-        description: 'Stay in charming cottages overlooking the breathtaking cliffs of El Nido.',
-        image: 'https://example.com/el-nido-cliffside-cottages.jpg',
-        url: 'https://example.com/el-nido-cliffside-cottages'
-    },
-    {
-        name: "Zhaya's Beach and Cottages",
-        location: 'El Nido',
-        price: 'p1350/night',
-        rating: 4.6,
-        description: 'Relax and unwind in beachfront cottages surrounded by tropical paradise.',
-        image: 'https://example.com/zhayas-beach-and-cottages.jpg',
-        url: 'https://example.com/zhayas-beach-and-cottages'
-    },
-];
+    var database = [
+        {
+            name: 'Boracay Beach Resort',
+            location: 'Boracay',
+            price: 'Php 1336/night',
+            rating: 4.2,
+            description: 'A beautiful beachfront resort located in the heart of Boracay.',
+            image: 'https://example.com/boracay-beach-resort.jpg',
+            url: 'https://example.com/boracay-beach-resort'
+        },
+        {
+            name: 'Nigi Nigi Too',
+            location: 'Boracay',
+            price: 'Php 3051/night',
+            rating: 4.2,
+            description: 'A cozy beachfront hotel offering comfortable accommodations and stunning views.',
+            image: 'https://example.com/nigi-nigi-too.jpg',
+            url: 'https://example.com/nigi-nigi-too'
+        },
+        {
+            name: 'Alta Vista de Boracay',
+            location: 'Boracay',
+            price: 'Php 2407/night',
+            rating: 4.0,
+            description: 'Experience luxury and relaxation at this hilltop resort in Boracay.',
+            image: 'https://example.com/alta-vista-de-boracay.jpg',
+            url: 'https://example.com/alta-vista-de-boracay'
+        },
+        {
+            name: 'Chill-Out Hostel Boracay',
+            location: 'Boracay',
+            price: 'Php 1323/night',
+            rating: 4.2,
+            description: 'A budget-friendly hostel located just a few steps from the beach.',
+            image: 'https://example.com/chill-out-hostel.jpg',
+            url: 'https://example.com/chill-out-hostel'
+        },
+        {
+            name: 'El Nido Cliffside Cottages',
+            location: 'El Nido',
+            price: 'Php 897/night',
+            rating: 4.0,
+            description: 'Stay in charming cottages overlooking the breathtaking cliffs of El Nido.',
+            image: 'https://example.com/el-nido-cliffside-cottages.jpg',
+            url: 'https://example.com/el-nido-cliffside-cottages'
+        },
+        {
+            name: "Zhaya's Beach and Cottages",
+            location: 'El Nido',
+            price: 'Php 1350/night',
+            rating: 4.6,
+            description: 'Relax and unwind in beachfront cottages surrounded by tropical paradise.',
+            image: 'https://example.com/zhayas-beach-and-cottages.jpg',
+            url: 'https://example.com/zhayas-beach-and-cottages'
+        },
+    ];
 
     // Filter hotels based on the destination
-    var filteredHotels = database.filter(function(hotel) {
+    var filteredHotels = database.filter(function (hotel) {
         return hotel.location.toLowerCase().includes(destination.toLowerCase());
     });
 
     // Sort hotels based on price in ascending order
-    filteredHotels.sort(function(a, b) {
+    filteredHotels.sort(function (a, b) {
         return a.price - b.price;
     });
 
@@ -80,7 +80,7 @@ function performHotelSearch(destination, checkIn, checkOut) {
     resultsContainer.innerHTML = '';
 
     // Display the filtered and sorted hotels
-    filteredHotels.forEach(function(hotel) {
+    filteredHotels.forEach(function (hotel) {
         var hotelElement = document.createElement('div');
         hotelElement.classList.add('hotel');
 
@@ -112,15 +112,15 @@ function performHotelSearch(destination, checkIn, checkOut) {
     // Add event listeners to the dropdown buttons
     var dropdownBtns = document.getElementsByClassName('dropdown-btn');
     for (var i = 0; i < dropdownBtns.length; i++) {
-        dropdownBtns[i].addEventListener('mouseenter', function() {
+        dropdownBtns[i].addEventListener('mouseenter', function () {
             this.innerText = 'Book a Flight';
         });
 
-        dropdownBtns[i].addEventListener('mouseleave', function() {
+        dropdownBtns[i].addEventListener('mouseleave', function () {
             this.innerText = 'Show Details';
         });
 
-        dropdownBtns[i].addEventListener('click', function() {
+        dropdownBtns[i].addEventListener('click', function () {
             var hotelUrl = this.dataset.url;
             window.location.href = hotelUrl;
         });
